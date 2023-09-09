b95cc7764f56d18e27c3c05876847b6ee94aa552
angelnieto18 <angelnietomatamoros@gmail.com>
2023-09-09 18:49:34 -0400

    feat: libraria tailwind agregada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>Hola</div>
-    </>
-  )
+	return (
+		<>
+			<header>
+				<NavigationMenu.Root className='flex'>
+					<NavigationMenu.List>
+						<NavigationMenu.Item>
+							<NavigationMenu.Link className='text-3xl'>MyBlog</NavigationMenu.Link>
+						</NavigationMenu.Item>
+					</NavigationMenu.List>
+					<NavigationMenu.List>
+						<NavigationMenu.Item>
+							<NavigationMenu.Link>Sign up</NavigationMenu.Link>
+						</NavigationMenu.Item>
+						<NavigationMenu.Item>
+							<NavigationMenu.Link>Log in</NavigationMenu.Link>
+						</NavigationMenu.Item>
+					</NavigationMenu.List>
+				</NavigationMenu.Root>
+			</header>
+		</>
+	);
 }
 
-export default App
+export default App;

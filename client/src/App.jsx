29a92b783ab1345932973d2b719c0798de3cb9c8
angel <angelnietomatamoros@gmail.com>
2023-09-29 13:57:02 -0400

    radix removido

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,10 @@
 import './index.css'
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
 function App() {
 	return (
 		<>
 			<header>
-				<NavigationMenu.Root className='flex'>
-					<NavigationMenu.List>
-						<NavigationMenu.Item>
-							<NavigationMenu.Link className='text-3xl'>MyBlog</NavigationMenu.Link>
-						</NavigationMenu.Item>
-					</NavigationMenu.List>
-					<NavigationMenu.List>
-						<NavigationMenu.Item>
-							<NavigationMenu.Link>Sign up</NavigationMenu.Link>
-						</NavigationMenu.Item>
-						<NavigationMenu.Item>
-							<NavigationMenu.Link>Log in</NavigationMenu.Link>
-						</NavigationMenu.Item>
-					</NavigationMenu.List>
-				</NavigationMenu.Root>
+				
 			</header>
 		</>
 	);
